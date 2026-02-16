8ebd97c9b07ee3e9a80271dcb4dede7d5b9783f4
ssmaniot <ssmaniot@gmail.com>
2026-02-16 19:38:06 +0100

    Refactor pagerank source data compression

--- src/pagerank.c
+++ src/pagerank.c
@@ -13,29 +13,26 @@
 
 #include "utils.h"
 
+/* Data to save/load CSR matrix */
+char fname[FNAME] = {0};
+char dir[DNAME] = {0};
+char row_ptr_p[PATH] = {0};
+char col_ind_p[PATH] = {0};
+char val_p[PATH] = {0};
+char danglings_p[PATH] = {0};
+char csr_data_p[PATH] = {0};
+char fres[PATH] = {0};
+CSR_data csr_data = {0};
+
+void perform_compression(const char dataset_path[FNAME]);
+
 int main(int argc, char *argv[]) {
-  /* Data to save/load CSR matrix */
   FILE *pdata;
-  char fname[FNAME];
-  char dir[DNAME];
-  char row_ptr_p[PATH];
-  char col_ind_p[PATH];
-  char val_p[PATH];
-  char danglings_p[PATH];
-  char csr_data_p[PATH];
-  CSR_data csr_data;
   struct stat st = {0};
 
   /* Reading data from input file */
-  FILE *pf;
   int no_nodes, no_edges;
-  char *s = NULL;
-  size_t slen = 0;
   ssize_t bytes;
-  int ri, ci;
-  int *from, *to;
-  int f, t;
-  int i, j;
 
   /* CSR matrix representation */
   double *val = NULL;
@@ -50,14 +47,14 @@ int main(int argc, char *argv[]) {
   double d;
   double dist;
   int iter;
-  char fres[PATH];
+  int ri, ci;
+  int i, j;
 
   /* Time elapsed data */
   clock_t begin, end;
   double elapsed_time;
 
   /* Extra data */
-  int *out_links;
   double sum;
   int err;
 
@@ -96,147 +93,7 @@ int main(int argc, char *argv[]) {
   /* Check if input data has already been compressed.
    * If data has NOT yet been compressed, then perform compression */
   if (stat(dir, &st) == -1) {
-    printf(
-        "Input file data \"%s\" is not compressed, ready to perform "
-        "compression...\n\n",
-        argv[1]);
-    begin = clock();
-    mkdir(dir, 0700);
-
-    if ((pf = fopen(argv[1], "r")) == NULL) {
-      fprintf(stderr, " [ERROR] Cannot open input file \"%s\"\n", argv[1]);
-      exit(EXIT_FAILURE);
-    }
-
-    /* Parsing input file header */
-    printf("Parsing input data...\n");
-    bytes = getline(&s, &slen, pf);
-    bytes = getline(&s, &slen, pf);
-    bytes = getline(&s, &slen, pf);
-    sscanf(s, "# Nodes: %d Edges: %d", &no_nodes, &no_edges);
-    printf("This graph has %d nodes and %d edges\n", no_nodes, no_edges);
-    bytes = getline(&s, &slen, pf);
-
-    csr_data.no_nodes = no_nodes;
-    csr_data.no_edges = no_edges;
-
-    /* Reading data from input file */
-    i = 0;
-    from = (int *)malloc(sizeof(int) * no_edges);
-    to = (int *)malloc(sizeof(int) * no_edges);
-    out_links = (int *)calloc(no_nodes, sizeof(int));
-    while ((bytes = getline(&s, &slen, pf)) != -1) {
-      sscanf(s, "%d %d", from + i, to + i);
-      out_links[from[i]] += 1;
-      if (i % 10 == 0) printf("\rEdge %d/%d", i, no_edges);
-      ++i;
-    }
-    printf("\rEdge %d/%d\n", i, no_edges);
-    printf("Done\n\n");
-    fclose(pf);
-    free(s);
-
-    /* Keeping track of danglings data */
-    no_danglings = 0;
-    for (i = 0; i < no_nodes; ++i)
-      if (out_links[i] == 0) ++no_danglings;
-    danglings = (int *)malloc(sizeof(int) * no_danglings);
-    j = 0;
-    for (i = 0; i < no_nodes; ++i)
-      if (out_links[i] == 0) danglings[j++] = i;
-
-    csr_data.no_danglings = no_danglings;
-
-    printf("Sorting edges...\n");
-    sort_input_data(from, to, no_edges);
-    printf("Done.\n\n");
-
-    /* csr matrix initialization */
-    val = (double *)malloc(sizeof(double) * no_edges);
-    col_ind = (int *)malloc(sizeof(int) * no_edges);
-    row_ptr = (int *)malloc(sizeof(int) * (no_nodes + 1));
-    ri = 0;
-    row_ptr[ri] = 0;
-    ci = 0;
-
-    /* Writing data in CSR matrix */
-    for (ci = 0; ci < no_edges; ++ci) {
-      f = from[ci];
-      t = to[ci];
-      if (t > ri) {
-        for (i = ri + 1; i <= t; ++i) row_ptr[i] = ci;
-        ri = t;
-      }
-      val[ci] = 1. / (double)out_links[f];
-      col_ind[ci] = f;
-    }
-    while (ri < no_nodes) row_ptr[++ri] = ci;
-
-    printf("CSR matrix filled\n");
-
-#ifdef DEBUG
-    printf("CSR Transposed matrix\n");
-    printf("---------------------\n");
-    printf("val:     [ ");
-    for (i = 0; i < no_edges; ++i) printf("%.3f ", val[i]);
-    printf("]\n");
-
-    printf("col_ind: [ ");
-    for (i = 0; i < no_edges; ++i) printf("%d ", col_ind[i]);
-    printf("]\n");
-
-    printf("row_ptr: [ ");
-    for (i = 0; i < no_nodes + 1; ++i) printf("%d ", row_ptr[i]);
-    printf("]\n\n");
-    printf("danglings: [ ");
-    for (j = 0; j < no_danglings; ++j) {
-      printf("%d", danglings[j]);
-      if (j < no_danglings - 1) printf(", ");
-    }
-    printf(" ]\n");
-    printf("Number of danglings nodes: %d\n\n", no_danglings);
-#endif
-
-    /* Writing data back to memory */
-    err = (write_data(row_ptr_p, (void *)row_ptr, sizeof(int), no_nodes + 1) ==
-           EXIT_FAILURE) ||
-          (write_data(col_ind_p, (void *)col_ind, sizeof(int), no_edges) ==
-           EXIT_FAILURE) ||
-          (write_data(val_p, (void *)val, sizeof(double), no_edges) ==
-           EXIT_FAILURE) ||
-          (write_data(danglings_p, (void *)danglings, sizeof(int),
-                      no_danglings) == EXIT_FAILURE) ||
-          (write_data(csr_data_p, (void *)&csr_data, sizeof(CSR_data), 1) ==
-           EXIT_FAILURE);
-
-    /* Input data */
-    free(from);
-    free(to);
-    from = NULL;
-    to = NULL;
-    /* Danglings data */
-    free(out_links);
-    free(danglings);
-    out_links = NULL;
-    danglings = NULL;
-    /* CSR data structure */
-    free(val);
-    free(col_ind);
-    free(row_ptr);
-    val = NULL;
-    col_ind = NULL;
-    row_ptr = NULL;
-
-    /* Manage error from writing data to memory */
-    if (err) {
-      delete_folder(dir);
-      fprintf(stderr, " [ERROR] Data could not be written in memory.\n");
-      exit(EXIT_FAILURE);
-    }
-    printf("Data written successfully!\n");
-
-    elapsed_time = (double)(clock() - begin) / CLOCKS_PER_SEC;
-    printf("Elapsed time: %.3fs\n\n", elapsed_time);
+    perform_compression(argv[1]);
   }
 
   /* Reading CSR matrix metadata info from file */
@@ -245,6 +102,7 @@ int main(int argc, char *argv[]) {
   bytes = fread(&no_nodes, sizeof(csr_data.no_nodes), 1, pdata);
   bytes = fread(&no_edges, sizeof(csr_data.no_edges), 1, pdata);
   bytes = fread(&no_danglings, sizeof(csr_data.no_danglings), 1, pdata);
+  (void)bytes;
   fclose(pdata);
   printf("no_nodes: %d\nno_edges: %d\nno_danglings: %d\n", no_nodes, no_edges,
          no_danglings);
@@ -388,4 +246,174 @@ int main(int argc, char *argv[]) {
   }
 
   return EXIT_SUCCESS;
+}
+
+void perform_compression(const char dataset_path[FNAME]) {
+  /* Reading data from input file */
+  FILE *pf;
+  int no_nodes, no_edges;
+  char *s = NULL;
+  size_t slen = 0;
+  ssize_t bytes;
+  int ri, ci;
+  int *from, *to;
+  int f, t;
+  int i, j;
+  int *out_links;
+
+  /* CSR matrix representation */
+  double *val = NULL;
+  int *col_ind = NULL;
+  int *row_ptr = NULL;
+
+  /* Pagerank computation data */
+  int *danglings = NULL;
+  int no_danglings;
+  int err;
+
+  /* Time elapsed data */
+  clock_t begin;
+  double elapsed_time;
+
+  printf(
+      "Input file data \"%s\" is not compressed, ready to perform "
+      "compression...\n\n",
+      dataset_path);
+  begin = clock();
+  mkdir(dir, 0700);
+
+  if ((pf = fopen(dataset_path, "r")) == NULL) {
+    fprintf(stderr, " [ERROR] Cannot open input file \"%s\"\n", dataset_path);
+    exit(EXIT_FAILURE);
+  }
+
+  /* Parsing input file header */
+  printf("Parsing input data...\n");
+  bytes = getline(&s, &slen, pf);
+  bytes = getline(&s, &slen, pf);
+  bytes = getline(&s, &slen, pf);
+  sscanf(s, "# Nodes: %d Edges: %d", &no_nodes, &no_edges);
+  printf("This graph has %d nodes and %d edges\n", no_nodes, no_edges);
+  bytes = getline(&s, &slen, pf);
+
+  csr_data.no_nodes = no_nodes;
+  csr_data.no_edges = no_edges;
+
+  /* Reading data from input file */
+  i = 0;
+  from = (int *)malloc(sizeof(int) * no_edges);
+  to = (int *)malloc(sizeof(int) * no_edges);
+  out_links = (int *)calloc(no_nodes, sizeof(int));
+  while ((bytes = getline(&s, &slen, pf)) != -1) {
+    sscanf(s, "%d %d", from + i, to + i);
+    out_links[from[i]] += 1;
+    if (i % 10 == 0) printf("\rEdge %d/%d", i, no_edges);
+    ++i;
+  }
+  printf("\rEdge %d/%d\n", i, no_edges);
+  printf("Done\n\n");
+  fclose(pf);
+  free(s);
+
+  /* Keeping track of danglings data */
+  no_danglings = 0;
+  for (i = 0; i < no_nodes; ++i)
+    if (out_links[i] == 0) ++no_danglings;
+  danglings = (int *)malloc(sizeof(int) * no_danglings);
+  j = 0;
+  for (i = 0; i < no_nodes; ++i)
+    if (out_links[i] == 0) danglings[j++] = i;
+
+  csr_data.no_danglings = no_danglings;
+
+  printf("Sorting edges...\n");
+  sort_input_data(from, to, no_edges);
+  printf("Done.\n\n");
+
+  /* csr matrix initialization */
+  val = (double *)malloc(sizeof(double) * no_edges);
+  col_ind = (int *)malloc(sizeof(int) * no_edges);
+  row_ptr = (int *)malloc(sizeof(int) * (no_nodes + 1));
+  ri = 0;
+  row_ptr[ri] = 0;
+  ci = 0;
+
+  /* Writing data in CSR matrix */
+  for (ci = 0; ci < no_edges; ++ci) {
+    f = from[ci];
+    t = to[ci];
+    if (t > ri) {
+      for (i = ri + 1; i <= t; ++i) row_ptr[i] = ci;
+      ri = t;
+    }
+    val[ci] = 1. / (double)out_links[f];
+    col_ind[ci] = f;
+  }
+  while (ri < no_nodes) row_ptr[++ri] = ci;
+
+  printf("CSR matrix filled\n");
+
+#ifdef DEBUG
+  printf("CSR Transposed matrix\n");
+  printf("---------------------\n");
+  printf("val:     [ ");
+  for (i = 0; i < no_edges; ++i) printf("%.3f ", val[i]);
+  printf("]\n");
+
+  printf("col_ind: [ ");
+  for (i = 0; i < no_edges; ++i) printf("%d ", col_ind[i]);
+  printf("]\n");
+
+  printf("row_ptr: [ ");
+  for (i = 0; i < no_nodes + 1; ++i) printf("%d ", row_ptr[i]);
+  printf("]\n\n");
+  printf("danglings: [ ");
+  for (j = 0; j < no_danglings; ++j) {
+    printf("%d", danglings[j]);
+    if (j < no_danglings - 1) printf(", ");
+  }
+  printf(" ]\n");
+  printf("Number of danglings nodes: %d\n\n", no_danglings);
+#endif
+
+  /* Writing data back to memory */
+  err = (write_data(row_ptr_p, (void *)row_ptr, sizeof(int), no_nodes + 1) ==
+         EXIT_FAILURE) ||
+        (write_data(col_ind_p, (void *)col_ind, sizeof(int), no_edges) ==
+         EXIT_FAILURE) ||
+        (write_data(val_p, (void *)val, sizeof(double), no_edges) ==
+         EXIT_FAILURE) ||
+        (write_data(danglings_p, (void *)danglings, sizeof(int),
+                    no_danglings) == EXIT_FAILURE) ||
+        (write_data(csr_data_p, (void *)&csr_data, sizeof(CSR_data), 1) ==
+         EXIT_FAILURE);
+
+  /* Input data */
+  free(from);
+  free(to);
+  from = NULL;
+  to = NULL;
+  /* Danglings data */
+  free(out_links);
+  free(danglings);
+  out_links = NULL;
+  danglings = NULL;
+  /* CSR data structure */
+  free(val);
+  free(col_ind);
+  free(row_ptr);
+  val = NULL;
+  col_ind = NULL;
+  row_ptr = NULL;
+
+  /* Manage error from writing data to memory */
+  if (err) {
+    delete_folder(dir);
+    fprintf(stderr, " [ERROR] Data could not be written in memory.\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Data written successfully!\n");
+
+  elapsed_time = (double)(clock() - begin) / CLOCKS_PER_SEC;
+  printf("Elapsed time: %.3fs\n\n", elapsed_time);
 }
